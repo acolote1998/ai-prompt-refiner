d107fbe761b0cfcf7e65b458c35d0aac7ed6d234
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-04 20:42:33 +0200

    invalidating query after updating the key

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,13 @@
 import { useUser } from "@clerk/clerk-react";
 import useGetKey from "../hooks/useGetKey";
 import { updateApiKeyByUserId } from "../supabaseCalls/useSupabase";
-
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 const Settings = () => {
   const { user } = useUser();
-
+  const [keyInput, setKeyInput] = useState<string>("");
   const { data: keyFromUser } = useGetKey(user?.id);
+  const queryClient = useQueryClient();
 
   if (!user) {
     return null; // or return a loading state
@@ -15,9 +17,19 @@ const Settings = () => {
     <div>
       <p>Settings</p>
       <p>Key: {keyFromUser?.key ?? "No key found"}</p>
+      <input
+        className="bg-white rounded-lg"
+        type="text"
+        onChange={(e) => {
+          setKeyInput(e.target.value);
+        }}
+      />
       <p
-        onClick={() => {
-          if (user) updateApiKeyByUserId(user.id, "usernewkey");
+        onClick={async () => {
+          if (user) {
+            await updateApiKeyByUserId(user.id, keyInput);
+            await queryClient.invalidateQueries({ queryKey: ["key"] });
+          }
         }}
       >
         UPDATE KEY
