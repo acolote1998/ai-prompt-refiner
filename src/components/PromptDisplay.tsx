70cbf8fb3a66fbcceb92485f39e2410bdcefadd1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-06 18:50:11 +0200

    styling prompt display component

--- src/components/PromptDisplay.tsx
+++ src/components/PromptDisplay.tsx
@@ -4,9 +4,9 @@ type PromptDisplayTipe = {
 };
 const PromptDisplay = ({ content, title }: PromptDisplayTipe) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{content}</p>
+    <div className="bg-gray-600 text-white pl-10 pr-10 rounded-lg m-2 pt-5 pb-5 flex flex-col gap-3">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <p className="rounded border-1">{content}</p>
     </div>
   );
 };
