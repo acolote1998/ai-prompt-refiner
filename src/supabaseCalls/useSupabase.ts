5b3f0031d3b3c4b75bcb8397e1e36f263f1e0d94
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-08-31 22:27:37 +0200

    removing commented code

--- src/supabaseCalls/useSupabase.ts
+++ src/supabaseCalls/useSupabase.ts
@@ -3,9 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-// const supabaseUrl = "https://rdipiauvmkkifzzfsznn.supabase.co";
-// const supabaseKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJkaXBpYXV2bWtraWZ6emZzem5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQyOTk4NzEsImV4cCI6MjA2OTg3NTg3MX0.2zuG2xzar4daak9NFbZHTdN1ymjzDv7Iz5pfTwiSjv4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getApiKeyByUserId = async (
